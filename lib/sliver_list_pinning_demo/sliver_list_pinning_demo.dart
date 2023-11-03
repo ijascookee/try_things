05efd8005077e4c0b8f6a3f0dd6df97a4d85e42e
Ijas Hussain <ijashussain@cookee.io>
2023-11-03 16:59:45 +0530

    change

---
+++ lib/sliver_list_pinning_demo/sliver_list_pinning_demo.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SliverListPinningDemo extends StatelessWidget {
+  const SliverListPinningDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
